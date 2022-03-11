6e4c50ab190d1eef02dd4cd7f8f3db98c8c420fe
turbotage <viktor.vigren@gmail.com>
2022-03-11 21:13:25 +0100

    fixed reference problem

--- FFI/mp_optim_interface.cpp
+++ FFI/mp_optim_interface.cpp
@@ -12,7 +12,7 @@ void ffi::model_create_from_type(ffi::ModelHandle** model_handle, int32_t modelt
 	switch (modeltype) {
 	case eMP_ModelType::ADC:
 		{
-			auto mh = *model_handle;
+			auto& mh = *model_handle;
 			mh = new ffi::ModelHandle;
 			mh->p_model = std::make_unique<tc::optim::MP_Model>(tc::models::mp_adc_eval_jac_hess, 
 				tc::models::mp_adc_diff, tc::models::mp_adc_diff2);
@@ -20,7 +20,7 @@ void ffi::model_create_from_type(ffi::ModelHandle** model_handle, int32_t modelt
 		break;
 	case eMP_ModelType::VFA:
 		{
-			auto mh = *model_handle;
+			auto& mh = *model_handle;
 			mh = new ffi::ModelHandle;
 			mh->p_model = std::make_unique<tc::optim::MP_Model>(tc::models::mp_vfa_eval_jac_hess,
 				tc::models::mp_vfa_diff, tc::models::mp_vfa_diff2);
@@ -28,7 +28,7 @@ void ffi::model_create_from_type(ffi::ModelHandle** model_handle, int32_t modelt
 		break;
 	case eMP_ModelType::T2:
 		{
-			auto mh = *model_handle;
+			auto& mh = *model_handle;
 			mh = new ffi::ModelHandle;
 			mh->p_model = std::make_unique<tc::optim::MP_Model>(tc::models::mp_t2_eval_jac_hess,
 				tc::models::mp_t2_diff, tc::models::mp_t2_diff2);
@@ -36,7 +36,7 @@ void ffi::model_create_from_type(ffi::ModelHandle** model_handle, int32_t modelt
 		break;
 	case eMP_ModelType::PSIR:
 		{
-			auto mh = *model_handle;
+			auto& mh = *model_handle;
 			mh = new ffi::ModelHandle;
 			mh->p_model = std::make_unique<tc::optim::MP_Model>(tc::models::mp_psir_eval_jac_hess,
 				tc::models::mp_psir_diff, tc::models::mp_psir_diff2);
@@ -44,7 +44,7 @@ void ffi::model_create_from_type(ffi::ModelHandle** model_handle, int32_t modelt
 		break;
 	case eMP_ModelType::IR:
 		{
-			auto mh = *model_handle;
+			auto& mh = *model_handle;
 			mh = new ffi::ModelHandle;
 			mh->p_model = std::make_unique<tc::optim::MP_Model>(tc::models::mp_irmag_eval_jac_hess,
 				tc::models::mp_irmag_diff, tc::models::mp_irmag_diff2);
@@ -57,7 +57,7 @@ void ffi::model_create_from_type(ffi::ModelHandle** model_handle, int32_t modelt
 
 void ffi::model_create_from_expr(ffi::ModelHandle** model_handle, const char* expression, const char** parameters, int num_parameters, const char** constants, int num_constants)
 {
-	auto mh = *model_handle;
+	auto& mh = *model_handle;
 
 	std::string expr(expression);
 	std::vector<std::string> params(&parameters[0], &parameters[num_parameters]);
@@ -86,7 +86,11 @@ void ffi::model_set_parameters(ffi::ModelHandle* model_handle, torch::Tensor* pa
 
 void ffi::model_set_constants(ffi::ModelHandle* model_handle, torch::Tensor** constants, int num_constants)
 {
-	std::vector<torch::Tensor> consts(constants[0], constants[num_constants]);
+	std::vector<torch::Tensor> consts(num_constants);
+	for (int i = 0; i < num_constants; ++i) {
+		consts[i] = *constants[i];
+	}
+
 	model_handle->p_model->constants() = consts;
 }
 
@@ -130,7 +134,7 @@ void ffi::optim_free(ffi::OptimHandle* optim_handle)
 
 void ffi::optim_run(ffi::OptimRunHandle** model_run_handle, ffi::OptimHandle* optim_handle, uint32_t iter)
 {
-	auto mrh = *model_run_handle;
+	auto& mrh = *model_run_handle;
 	mrh = new ffi::OptimRunHandle;
 	mrh->task_future = std::async(std::launch::async, [optim_handle, iter]() {
 			optim_handle->p_optim_handle->run(iter); });
@@ -169,31 +173,31 @@ void ffi::optim_get_info(ffi::OptimHandle* optim_handle, ffi::OptimRunHandle* op
 
 void ffi::get_plane_converging_problems_combined(torch::Tensor** cp, torch::Tensor* lastJ, torch::Tensor* lastP, torch::Tensor* lastR, float tolerance)
 {
-	auto c = *cp;
+	auto& c = *cp;
 	c = new torch::Tensor(tc::optim::get_plane_converging_problems_combined(*lastJ, *lastP, *lastR, tolerance));
 }
 
 void ffi::get_plane_converging_problems(torch::Tensor** cp, torch::Tensor* lastJ, torch::Tensor* lastP, torch::Tensor* lastR, float tolerance)
 {
-	auto c = *cp;
+	auto& c = *cp;
 	c = new torch::Tensor(tc::optim::get_plane_converging_problems(*lastJ, *lastP, *lastR, tolerance));
 }
 
 void ffi::get_gradient_converging_problems_absolute(torch::Tensor** cp, torch::Tensor* J, torch::Tensor* R, float tolerance)
 {
-	auto c = *cp;
+	auto& c = *cp;
 	c = new torch::Tensor(tc::optim::get_gradient_converging_problems_absolute(*J, *R, tolerance));
 }
 
 void ffi::get_gradient_converging_problems_relative(torch::Tensor** cp, torch::Tensor* J, torch::Tensor* R, float tolerance)
 {
-	auto c = *cp;
+	auto& c = *cp;
 	c = new torch::Tensor(tc::optim::get_gradient_converging_problems_relative(*J, *R, tolerance));
 }
 
 void ffi::get_gradient_converging_problems_combined(torch::Tensor** cp, torch::Tensor* J, torch::Tensor* R, float tolerance)
 {
-	auto c = *cp;
+	auto& c = *cp;
 	c = new torch::Tensor(tc::optim::get_gradient_converging_problems_combined(*J, *R, tolerance));
 }
 
@@ -224,7 +228,7 @@ void ffi::strp_create(ffi::OptimHandle** optim_handle, ffi::ModelHandle* model_h
 	tc::optim::MP_OptimizerSettings optsettings(std::move(mod), *data);
 	tc::optim::MP_STRPSettings strpsettings(std::move(optsettings), rJ.first, rJ.second, delta, scaling, mu, eta);
 
-	auto oh = *optim_handle;
+	auto& oh = *optim_handle;
 	oh = new ffi::OptimHandle;
 
 	auto mpp = tc::optim::MP_STRP::make(std::move(strpsettings));
